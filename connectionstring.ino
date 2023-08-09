d149e23f560cccd3ed3e5a9262b707a872fc0b82
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2023-08-08 21:22:12 -0600

    Update connectionstring.ino

--- connectionstring.ino
+++ connectionstring.ino
@@ -2,3 +2,9 @@ HostName=FleetManagerHub-yong.azure-devices.net;SharedAccessKeyName=iothubowner;
 
 
   HostName=FleetManagerHub-yong.azure-devices.net;DeviceId=DHT11_temperature_humidity_sensor;SharedAccessSignature=SharedAccessSignature sr=FleetManagerHub-yong.azure-devices.net%2Fdevices%2FDHT11_temperature_humidity_sensor&sig=zWYm1QP0qDgTHQzUVVZ6RE97ZesjvAAMSUwidMt%2B4U0%3D&se=1691551499
+
+
+
+  HostName=FleetManagerHub-yong.azure-devices.net;DeviceId=DHT11_temperature_humidity_sensor;SharedAccessSignature=SharedAccessSignature sr=FleetManagerHub-yong.azure-devices.net%2Fdevices%2FDHT11_temperature_humidity_sensor&sig=4%2FNLszIgjhaqg4wqGd3Yc1GSrDkaLt9vr99ARccpJrM%3D&se=1691557292
+  
+  
